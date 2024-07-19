b5888c869e75d8eb911d6aba1d6b863a46b71f6a
gricelbarrera <gbarrera@redhat.com>
2024-07-19 10:26:23 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+def main(nombre):
+    if nombre=="gricel":
+        print("estudiante")
+    elif nombre=="matheus":
+        print("mentor")
+    else:
+        print("No conozco")
+    return
+
+
+if __name__ == "__main__":
+    main("jose")
